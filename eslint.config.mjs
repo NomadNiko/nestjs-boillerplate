498bd642ad77317ea459c8547359b6ec06a09264
NomadNiko <nomad.niko.inc@gmail.com>
2025-03-09 15:30:20 -1000

    build: adjusting configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -41,8 +41,6 @@ export default [
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error'],
       'require-await': 'off',
       '@typescript-eslint/require-await': 'error',
       '@typescript-eslint/no-floating-promises': 'error',
